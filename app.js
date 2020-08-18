d3a89733a38c865082d3f78ec35ea6dbf9549cd6
Gustavo Furtado de Oliveira Alves <gustavo.alves@gsw.com.br>
2020-08-18 20:47:15 -0300

    Add cors to express.

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.json());
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-    console.log('GET /')
+    console.log('GET /', req.headers)
     console.log('Headers:')
     console.log(req.headers['connection'])
-    res.header('location', 'http://dicasdeprogramacao.com.br/')
+    res.header('location', 'http://dicasdejavascript.com.br/')
     res.status(307).send()
 })
 
 app.post('/clientes', (req, res) => {
     console.log('POST /clientes', req.body)
-    console.log("tem email? ", !req.body['email'])
+    console.log("tem email? ", req.body['email'])
     if(!req.body['email']){
         res.status(400).send("O email é obrigatório.")
     } else {
-        res.send('OK')
+        res.json('{"nome":"Gustavo"}')
     }
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Rodando aplicação no endereço http://localhost:${port}`)
 })
